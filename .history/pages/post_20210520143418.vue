643a8a4eaec8a1004fc26e672b0fe3a38dc2e406
Ian Fan <ian@pollex.com.tw>
2021-05-20 14:35:56 +0800

    update

---
+++ .history/pages/post_20210520143418.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="mountain in mountains" :key="mountain.title">
+        <NuxtLink :to="{ name: 'mountains-slug', params: { slug: mountain.slug } }">
+          {{ mountain.title }}
+        </NuxtLink>
+        <Nuxt />
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+  async asyncData () {
+    const mountains = await axios.get('https://api.nuxtjs.dev/mountains')
+    return { mountains }
+  }
+}
+</script>
+<style lang="scss">
+
+</style>
---
+++ .history/pages/post_20210520143524.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="mountain in mountains" :key="mountain.title">
+        <NuxtLink :to="{ name: 'mountains-slug', params: { slug: mountain.slug } }">
+          {{ mountain.title }}
+        </NuxtLink>
+        <Nuxt />
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  async asyncData ({ $axios }) {
+    const mountains = await $axios.$get('https://api.nuxtjs.dev/mountains')
+    return { mountains }
+  }
+}
+</script>
+<style lang="scss">
+
+</style>
